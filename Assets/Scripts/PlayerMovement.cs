23760cbbd46844cfecb63dd6bf3927b9941dd659
starbasil143 <167671623+starbasil143@users.noreply.github.com>
2024-10-15 20:09:47 -0500

    add basic movement system, placeholder sprites

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    private Vector2 _movement;
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    private const string  _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private const string _lastHorizontal = "LastHorizontal";
+    private const string _lastVertical = "LastVertical";
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        
+    }
+
+    private void Update()
+    {
+        _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        _rigidbody.velocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
+
+        if (_movement != Vector2.zero)
+        {
+            _animator.SetFloat(_lastHorizontal, _movement.x);
+            _animator.SetFloat(_lastVertical, _movement.y);
+        }
+    }
+}
