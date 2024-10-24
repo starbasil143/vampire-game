3a08c9318b1356d98502d786f25297bac49093ab
starbasil143 <167671623+starbasil143@users.noreply.github.com>
2024-10-24 05:26:48 -0500

    add title screen

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+    public bool toFadeIn;
+    public bool fadeImmediately;
+    public bool fadingNow;
+    public float timeBeforeFade;
+    public float fadeLength;
+    private float timer = 0f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (timer >= timeBeforeFade)
+        {
+            fadingNow = true;
+            Fade();
+        }
+        else
+        {   
+            timer += Time.deltaTime;    
+        }
+    }
+
+    public void Fade()
+    {
+        if (gameObject.GetComponent<SpriteRenderer>())
+        {
+
+        }
+    }
+}
